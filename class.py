0d0e27e2b62f7d389c6470ad67161699e4836035
tgiwa <giwatoheeb27@gmail.com>
2026-08-13 15:27:01 +0100

    class and oop

---
+++ class.py
@@ -0,0 +1,31 @@
+# creating class in OOP
+class MyInfo:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+printInfo = MyInfo("Giwa",16)
+print(printInfo)
+
+print(printInfo.name,printInfo.age)
+
+# Acess and modifying attribute
+
+class SchoolMateria:
+    def __init__(self):
+        self.book = "Stationary"
+        self.pen = "Lucky"
+
+giwaMateria = SchoolMateria()
+print(giwaMateria.pen,giwaMateria.book)
+print(giwaMateria.book)
+
+class MyInfo:
+    def __init__(self, name,age):
+        self.name = name
+        self.age = age
+    def __repr__(self):
+        return f'MyInfo({self.name},{self.age})'
+    
+person = repr(MyInfo("Giwas",22))
+print(person)
