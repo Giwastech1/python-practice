aad8b763474ec933477d32f12e834d6b2ca9fe76
tgiwa <giwatoheeb27@gmail.com>
2026-09-04 14:56:57 +0100

    zip iteration

--- sorts.py
+++ sorts.py
@@ -6,3 +6,22 @@ for i in range(n):
         if numbers[j] > numbers[j+1]:
             numbers[j],numbers[j+1] = numbers[j+1],numbers[j]
 print(numbers)
+
+# iter
+items = (2,4,5,7)
+
+iterator = iter(items)
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(type(iterator))
+
+
+# zip iterator
+cars = ["Mazda","Volvo","Toyota"]
+prices = [12000, 13500, 13000]
+
+for car_brand in zip(cars,prices):
+    print(f"car: {cars}, amount: {prices}")
+print(car_brand)
